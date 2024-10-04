3b657a24dc47e440b850e3900c1bb04144172c43
TWhite10 <136852211+TWhite10@users.noreply.github.com>
2024-10-04 13:30:02 -0400

    initial pop

--- index.js
+++ index.js
@@ -71,7 +71,7 @@
 // let output = csvFile(csvData);
 // console.log(output);
 
-//part 4 
+//part 4 - pop/remove bottom array and
 let csvData  = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
 
 function csvFile(csvString){
@@ -87,8 +87,12 @@ function csvFile(csvString){
         }
         arrData.push(objData);
     } 
+    arrData.pop()
     return arrData;
 } 
 let output = csvFile(csvData);
 console.log(output);
+
+//part 4 - push/add bottom array and
+
 
